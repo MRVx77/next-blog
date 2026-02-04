3b9c6aa8c3627ce569aba44236dbc1526641ec41
MRVx77 <vanshdakhode23@gmail.com>
2026-02-04 20:01:36 +0530

    feat: implement login form with validation and UI components

--- components/auth/login-from.tsx
+++ components/auth/login-from.tsx
@@ -1,5 +1,63 @@
+"use client";
+import { zodResolver } from "@hookform/resolvers/zod";
+import { useState } from "react";
+import { useForm } from "react-hook-form";
+import { z } from "zod";
+import { Form, FormControl, FormField, FormItem, FormLabel } from "../ui/form";
+import { Input } from "../ui/input";
+
+//schema
+const LoginSchema = z.object({
+  email: z.string().email("please enter a valid email address"),
+  password: z.string().min(6, "password must be at least 6 characters long"),
+});
+
+type LoginFormValues = z.infer<typeof LoginSchema>;
+
 function LoginForm() {
-  return <div>Login form </div>;
+  const [isLoading, setIsLoading] = useState(false);
+
+  const form = useForm<LoginFormValues>({
+    resolver: zodResolver(LoginSchema),
+    defaultValues: {
+      email: "",
+      password: "",
+    },
+  });
+  return (
+    <Form {...form}>
+      <form className="space-y-4">
+        <FormField
+          control={form.control}
+          name="email"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Email</FormLabel>
+              <FormControl>
+                <Input type="email" placeholder="Enter your email" {...field} />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+        <FormField
+          control={form.control}
+          name="password"
+          render={({ field }) => (
+            <FormItem>
+              <FormLabel>Password</FormLabel>
+              <FormControl>
+                <Input
+                  placeholder="Enter your password"
+                  type="password"
+                  {...field}
+                />
+              </FormControl>
+            </FormItem>
+          )}
+        />
+      </form>
+    </Form>
+  );
 }
 
 export default LoginForm;
