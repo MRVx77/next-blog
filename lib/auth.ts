12deb6112f3f91d03be51aab02d34d52763f344f
MRVx77 <vanshdakhode23@gmail.com>
2026-02-07 13:30:53 +0530

    feat: implement database schema and authentication setup with better-auth integration

--- lib/auth.ts
+++ lib/auth.ts
@@ -1,7 +1,43 @@
 import { betterAuth } from "better-auth";
+import { drizzleAdapter } from "better-auth/adapters/drizzle";
+import { db } from "./db";
+
+import * as schema from "./db/schema";
 
 export const auth = betterAuth({
   appName: "NextBlog",
   secret: process.env.BETTER_AUTH_SECRET || " BETTER_AUTH_SECRET",
   baseURL: process.env.BASE_URL || "http://localhost:3000",
+  database: drizzleAdapter(db, {
+    provider: "pg",
+    schema: {
+      ...schema,
+      user: schema.users,
+      session: schema.sessions,
+      account: schema.accounts,
+    },
+  }),
+  emailAndPassword: {
+    enabled: true,
+    requireEmailVerification: false,
+    minPasswordLength: 6,
+    maxPasswordLength: 72,
+    autoSignIn: false,
+  },
+  session: {
+    expiresIn: 60 * 60 * 24 * 7, // 7 days
+    updateAge: 60 * 60 * 24, // 1 day
+    cookieCache: {
+      enabled: true,
+      maxAge: 60 * 5, // 5 minutes
+    },
+    disableSessionRefresh: false,
+  },
+  advanced: {
+    useSecureCookies: process.env.NODE_ENV === "production",
+    defaultCookieAttributes: {
+      httpOnly: true,
+      secure: process.env.NODE_ENV === "production",
+    },
+  },
 });
---
+++ lib/db/schema.ts
@@ -0,0 +1,94 @@
+import { relations } from "drizzle-orm";
+import {
+  boolean,
+  pgTable,
+  text,
+  timestamp,
+  varchar,
+} from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  name: varchar("name", { length: 255 }).notNull(),
+  email: varchar("email", { length: 255 }).unique(),
+  emailVerified: boolean("email_verified").default(false),
+  createAt: timestamp("created_at").defaultNow().notNull(),
+  updateAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const sessions = pgTable("sessions", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  userId: varchar("user_id", { length: 255 })
+    .references(() => users.id)
+    .notNull(),
+  token: varchar("token", { length: 255 }).notNull(),
+  expiresAt: timestamp("expires_at"),
+  ipAddress: varchar("ip_address", { length: 255 }),
+  userAgent: text("user_agent"),
+  createAt: timestamp("created_at").defaultNow().notNull(),
+  updateAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const accounts = pgTable("accounts", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  userId: varchar("user_id", { length: 255 })
+    .references(() => users.id)
+    .notNull(),
+  accountId: varchar("account_id", { length: 255 }).notNull(),
+  providerId: varchar("provider_id", { length: 255 }).notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  createAt: timestamp("created_at").defaultNow().notNull(),
+  updateAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+export const posts = pgTable("posts", {
+  id: varchar("id", { length: 255 }).primaryKey(),
+  title: varchar("title", { length: 255 }).notNull(),
+  discription: text("description").notNull(),
+  slug: varchar("slug", { length: 255 }).notNull().unique(),
+  content: text("content").notNull(),
+  authorId: varchar("author_id", { length: 255 })
+    .references(() => users.id)
+    .notNull(),
+  createAt: timestamp("created_at").defaultNow().notNull(),
+  updateAt: timestamp("updated_at").defaultNow().notNull(),
+});
+
+// each user can have multiple posts, but each post belongs to one user
+export const userRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
+
+// each post belongs to one user, but a user can have multiple posts
+export const postRelations = relations(posts, ({ one }) => ({
+  author: one(users, {
+    fields: [posts.authorId],
+    references: [users.id],
+  }),
+}));
+
+// each account belongs to one user, but a user can have multiple accounts
+export const accountRelations = relations(accounts, ({ one }) => ({
+  user: one(users, {
+    fields: [accounts.userId],
+    references: [users.id],
+  }),
+}));
+
+// each session belongs to one user, but a user can have multiple sessions
+export const sessionRelations = relations(sessions, ({ one }) => ({
+  user: one(users, {
+    fields: [sessions.userId],
+    references: [users.id],
+  }),
+}));
+
+export const schema = {
+  users,
+  sessions,
+  accounts,
+  posts,
+};
